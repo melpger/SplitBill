da7dc25e8df30ec4166f301f8afd8f39af8c06b5
Melvin Geremillo <melvin.geremillo@awsys-i.com>
2024-06-06 08:39:13 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SplitBill',
+  webDir: 'www'
+};
+
+export default config;
