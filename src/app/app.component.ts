a09a057fe155d647211737bfdde5c2af5e46a653
Melvin Geremillo <melvin.geremillo@awsys-i.com>
2024-06-06 22:57:44 +0900

    firebase commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,18 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyArAxNYHJorhZWp-qx3zn_iHwdc_5fWPEc",
+      authDomain: "splityourbillnow.firebaseapp.com",
+      projectId: "splityourbillnow",
+      storageBucket: "splityourbillnow.appspot.com",
+      messagingSenderId: "832541768215",
+      appId: "1:832541768215:web:5a1d4add17f814fe6b7a52",
+      measurementId: "G-RHFXY6CCLG"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }
